4cc473373ad08582732a99ec4818565a97b7341e
Hardikk <kambojpushkal@gmail.com>
2023-09-30 12:50:08 +0530

    added copy functionality

--- src/colorize.js
+++ src/colorize.js
@@ -1,45 +1,63 @@
-// Desc: This is the main page of the colorizer app
-// this is a functional component
 import { useState } from "react";
 
 const Colorizer = () => {
+  const [color, setColor] = useState("#6699ff");
+  const [counter, setCounter] = useState(0);
+  const [copySuccess, setCopySuccess] = useState(false);
 
+  const getRandomColor = () => {
+    const random = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return random;
+  };
 
-    const [color, setColor] = useState('#6699ff');
-    const [counter, setCounter] = useState(0);
+  const changeColor = () => {
+    const randomColor = getRandomColor();
+    console.log("Clicked and color changed with random color", randomColor);
+    setColor(randomColor);
+    increaseCounter();
+  };
 
-    const getRandomColor = () => {
-        const random = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-        return random;
+  const increaseCounter = () => {
+    console.log("Increased by 1");
+    setCounter((prev) => prev + 1);
+  };
+
+  const resetCounter = () => {
+    setCounter(0);
+  };
+
+  const copyColorToClipboard = () => {
+    navigator.clipboard.writeText(color).then(
+      function () {
+        // Success callback: Update state to show the success message.
+        setCopySuccess(true);
+
+        // Reset the success message after a delay (e.g., 2 seconds).
+        setTimeout(() => {
+          setCopySuccess(false);
+        }, 2000);
+      },
+      function (err) {
+        // Error callback (optional): Handle errors here.
+        console.error("Error copying color to clipboard: ", err);
       }
-
-   
-    const changeColor = () => {
-        const randomColor = getRandomColor();
-        console.log('clicked and color changee with random color', randomColor);
-        setColor (randomColor);
-        increaseCounter();
-    }
-
-    const increaseCounter = () => {
-        console.log('incrBy1');
-        //setCounter(counter + 1);
-        setCounter((prev) => prev + 1);
-      }
-      
-    const resetCounter = () => {
-        setCounter(0);
-      };
-    return(
-        <div className="colorizer" style={{ backgroundColor : color }}>
-            <div className="counter"> 
-            <h1>You have changed the color {counter} times.</h1>
-            </div>
-            <div className="box" style={{ backgroundColor : color }}> {color} </div>
-                <button onClick={ changeColor }>Change color now!</button>
-                <button onClick={resetCounter}>Reset Counter</button>
-            </div>
     );
+  };
+
+  return (
+    <div className="colorizer" style={{ backgroundColor: color }}>
+      <div className="counter">
+        <h1>You have changed the color {counter} times.</h1>
+      </div>
+      <div className="box" style={{ backgroundColor: color }}>
+        {color}
+        <button onClick={copyColorToClipboard}>Copy!</button>
+        {copySuccess && <div className="copy-success">Color copied to clipboard!</div>}
+      </div>
+      <button onClick={changeColor}>Change color now!</button>
+      <button onClick={resetCounter}>Reset Counter</button>
+    </div>
+  );
 };
 
 export default Colorizer;
